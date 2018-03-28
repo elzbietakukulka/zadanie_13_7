809cd48827666c79e604d513d0cca5d21808c291
Elżbieta Kukułka <ellakaras@gmail.com>
2018-03-28 17:42:29 +0200

    Create function that opens a file and save data in another file

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var StatMode = require('stat-mode');
+var colors = require('colors');
+
+fs.stat('./cat.jpeg', function(err, stats) {
+    var statMode = new StatMode(stats);
+    console.log(statMode.toString());
+});
+
+/*fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
+});*/
+
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    var text = data;
+    fs.writeFile('./tekstDwa.txt', text, function(err) {
+        if (err) throw err;
+        fs.readFile('./tekstDwa.txt', 'utf-8', function(err, data) {
+            console.log(data);
+        });
+    });
+});
